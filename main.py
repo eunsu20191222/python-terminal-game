2817b2e693ca99c1032c5b9a37e1ae66a144e171
eunsu20191222 <eunsuvg@gmail.com>
2021-05-25 20:48:32 +0800

    에러 처리

--- main.py
+++ main.py
@@ -10,9 +10,13 @@ sleep(1)
 ask_for_i = input("게임을 플레이 하는 법을 보려면 i 를 클릭해 주세요, 바로 시작할려면 s 를 눌러주세요: ")
 
 if ask_for_i == "i":
-    print("this is instroduction")
+    sleep(1)
+    print("\nthis is instroduction")
+    sleep(3)
     game(user)
 
 elif ask_for_i == "s":
+    sleep(1)
     print("start game")
+    sleep(2)
     game(user)
