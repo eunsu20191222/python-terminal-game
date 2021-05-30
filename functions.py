6534ad8204f96c77943671fea1b527de2c0bca75
eunsu20191222 <eunsuvg@gmail.com>
2021-05-30 20:15:31 +0800

    끝

--- functions.py
+++ functions.py
@@ -8,11 +8,11 @@ def start(name):
 
 def game():
     farm = 1
-    print(f"농장: {farm}")
     mmoney = 0
     while True:
         mmoney += 100
         money = mmoney
+        print(f"농장: {farm}")
         print(f"돈: {money}")
         time.sleep(1)
         os.system("clear")
